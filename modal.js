2eafc3480e50d40527c04a7260281d13e315aba4
tapsikk <tapsgame@gmail.com>
2023-10-22 17:59:15 +0300

    modal all

---
+++ modal.js
@@ -0,0 +1,41 @@
+// Находим кнопки открытия и закрытия модального окна
+const openModalButton = document.getElementById("open-burger");
+const closeModalButton = document.getElementById("button-burger-close");
+
+// Находим модальное окно
+const modal = document.getElementById("modal");
+
+// Добавляем обработчики событий для открытия и закрытия модального окна
+openModalButton.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+closeModalButton.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Закрываем модальное окно, если пользователь щелкает за его пределами
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+const openModalButtonx2 = document.getElementById("buttonx2");
+const closeModalButtonx2 = document.getElementById("buttonx2-close");
+
+const modalx2 = document.getElementById("modalx2");
+
+openModalButtonx2.addEventListener("click", () => {
+    modalx2.style.display = "block";
+});
+
+closeModalButtonx2.addEventListener("click", () => {
+    modalx2.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
